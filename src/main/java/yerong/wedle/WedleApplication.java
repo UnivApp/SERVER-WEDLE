6ba49afb0306b6154d245af3e16de05a551de096
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-09-02 21:43:38 +0900

    Initial Commit

---
+++ src/main/java/yerong/wedle/WedleApplication.java
@@ -0,0 +1,13 @@
+package yerong.wedle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yerong/wedle/WedleApplicationTests.java
@@ -0,0 +1,13 @@
+package yerong.wedle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
