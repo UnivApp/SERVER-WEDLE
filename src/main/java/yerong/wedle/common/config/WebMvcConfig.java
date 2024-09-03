054dd94343c60d39838a0f94c9ec114899f27715
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-09-03 18:09:15 +0900

    feat: Swagger 설정 및 cors 설정

---
+++ src/main/java/yerong/wedle/common/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package yerong.wedle.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String url;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedOrigins(url, "https://appleid.apple.com")
+                .maxAge(3600);
+    }
+}
--- src/main/java/yerong/wedle/star/repository/StarRepository.java
+++ src/main/java/yerong/wedle/star/repository/StarRepository.java
@@ -3,21 +3,22 @@ package yerong.wedle.star.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import yerong.wedle.member.domain.Member;
 import yerong.wedle.star.domain.Star;
 import yerong.wedle.university.domain.University;
 
 public interface StarRepository extends JpaRepository<Star, Long> {
-
-    Long countByUniversityId(Long universityId);
+    @Query("SELECT COUNT(s) FROM Star s WHERE s.university.universityId = :universityId")
+    Long countByUniversityId(@Param("universityId") Long universityId);
 
     @Modifying
     @Query(value = "INSERT INTO star(member_id, university_id, createdAt) VALUES (:memberId, :universityId, now())", nativeQuery = true)
-    int mStar(Long memberId, Long universityId);
+    int mStar(@Param("memberId") Long memberId, @Param("universityId") Long universityId);
 
     @Modifying
-    @Query(value = "DELETE FROM star WHERE member_id = :memberId AND university_id = :university_id", nativeQuery = true)
-    int mUnStar(Long member_id, Long universityId);
+    @Query(value = "DELETE FROM star WHERE member_id = :memberId AND university_id = :universityId", nativeQuery = true)
+    int mUnStar(@Param("memberId") Long memberId, @Param("universityId") Long universityId);
 
     boolean existsByMemberAndUniversity(Member member, University university);
 
