7d3b54d30c36c5d88dbbc9e212004d9d91b649f1
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-09-04 20:25:35 +0900

    feat: 즐겨찾기 기능

--- src/main/java/yerong/wedle/star/controller/StarApiController.java
+++ src/main/java/yerong/wedle/star/controller/StarApiController.java
@@ -2,10 +2,12 @@ package yerong.wedle.star.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import yerong.wedle.oauth.princiapl.PrincipalDetails;
 import yerong.wedle.star.service.StarService;
 
 @RequiredArgsConstructor
@@ -16,13 +18,23 @@ public class StarApiController {
     private final StarService starService;
 
     @PostMapping("/add")
-    public ResponseEntity<Void> addStar(@RequestParam Long memberId, @RequestParam String universityName) {
+    public ResponseEntity<Void> addStar(
+            @RequestParam String universityName,
+            @AuthenticationPrincipal PrincipalDetails principalDetails
+            ) {
+
+        Long memberId = principalDetails.getMember().getMemberId();
+
         starService.addStar(memberId, universityName);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/remove")
-    public ResponseEntity<Void> removeStar(@RequestParam Long memberId, @RequestParam String universityName) {
+    public ResponseEntity<Void> removeStar(@RequestParam String universityName,
+                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        Long memberId = principalDetails.getMember().getMemberId();
+
         starService.removeStar(memberId, universityName);
         return ResponseEntity.ok().build();
     }
