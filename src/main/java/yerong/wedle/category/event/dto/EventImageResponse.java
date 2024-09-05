00da6f6fb986ac64b528c2a990c87c4dd69f4c9f
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-09-05 22:46:57 +0900

    refactor: 행사 리팩토리

---
+++ src/main/java/yerong/wedle/category/event/dto/EventImageResponse.java
@@ -0,0 +1,13 @@
+package yerong.wedle.category.event.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class EventImageResponse {
+    private String url;
+    private String source;
+}
