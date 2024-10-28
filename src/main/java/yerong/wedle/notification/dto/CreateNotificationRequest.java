e5a4557c347643e17033a5f29a756be2468d5955
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-10-28 16:31:30 +0900

    feat: 알림 구현 리팩토링

--- src/main/java/yerong/wedle/notification/dto/CreateNotificationRequest.java
+++ src/main/java/yerong/wedle/notification/dto/CreateNotificationRequest.java
@@ -1,16 +1,19 @@
 package yerong.wedle.notification.dto;
 
 import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import org.checkerframework.checker.units.qual.N;
 
 @Getter
 @Setter
 public class CreateNotificationRequest {
     @NotNull
-    private LocalDateTime notificationTime;
+    private LocalDate notificationDate;;
 
     @NotNull
     private Long eventId;
