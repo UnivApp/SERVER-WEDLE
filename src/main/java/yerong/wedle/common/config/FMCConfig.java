4a3539556a4154fb22294f7c247fe7520a1bd584
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-10-28 17:35:58 +0900

    feat: firebase 등록

--- src/main/java/yerong/wedle/common/config/FMCConfig.java
+++ src/main/java/yerong/wedle/common/config/FMCConfig.java
@@ -7,15 +7,18 @@ import com.google.firebase.FirebaseOptions;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class FMCConfig {
 
+    @Value("${FIREBASE_CONFIG}")
+    private String firebaseConfig;
+
     @PostConstruct
     private void init() throws IOException {
-        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
         ByteArrayInputStream serviceAccountStream = new ByteArrayInputStream(firebaseConfig.getBytes());
 
         FirebaseOptions options = FirebaseOptions.builder()
